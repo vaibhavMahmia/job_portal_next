c7d47edb28444e2d8573f2aa475e34acbc8a46f7
vaibhav mahmia <vaibhav@kali.kali>
2025-11-04 00:04:27 +0530

    refresh session expiry time when 15 days remaining

--- src/features/auth/server/use_cases/sessions.ts
+++ src/features/auth/server/use_cases/sessions.ts
@@ -3,7 +3,7 @@ import { getIPAddress } from './location';
 import { cookies, headers } from 'next/headers';
 import { db } from '@/config/db';
 import { sessions, users } from '@/drizzle/schema';
-import { SESSION_LIFETIME } from '@/config/constant';
+import { SESSION_LIFETIME, SESSION_REFRESH_RATE } from '@/config/constant';
 import { eq } from 'drizzle-orm';
 
 type CreateSessionData = {
@@ -37,6 +37,8 @@ export const createSessionAndSetCookies = async (userId: number) => {
     cookieStore.set('session', token, { secure: true, httpOnly: true, maxAge: SESSION_LIFETIME });
 }
 
+const invalidateSession =  async (id: string) => await db.delete(sessions).where(eq(sessions.id, id));
+
 export const validateSessionAndGetUser = async (session: string) => {
     const hashedToken = crypto.createHash('sha-256').update(session).digest('hex');
     const [user] = await db.select({
@@ -55,6 +57,16 @@ export const validateSessionAndGetUser = async (session: string) => {
         crestedAt: users.createdAt,
         updatedAt: users.updatedAt
     }).from(sessions).where(eq(sessions.id, hashedToken)).innerJoin(users, eq(users.id, sessions.userId));
+
+    if(!user) return null;
+
+    if(Date.now() >= user.session.expiresAt.getTime()){
+        await invalidateSession(user.session.id);
+        return null;
+    }
+
+    if(Date.now() >= user.session.expiresAt.getTime() - SESSION_REFRESH_RATE * 1000)
+        await db.update(sessions).set({ expiresAt: new Date(Date.now() + SESSION_LIFETIME * 1000) }).where(eq(sessions.id, user.session.id));
 
     return user;
 }
